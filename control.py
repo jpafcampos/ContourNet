d10cb49a28ebfd9cb6c0d2e56aefa3287d72bfb7
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2021-03-27 22:07:25 +0100

    update

--- control.py
+++ control.py
@@ -81,7 +81,6 @@ class control():
                 torch.cuda.empty_cache()
                 x = x.to(flags.device)
                 y = y.to(flags.device)
-                print(y)
 
                 results = self.net(x)
                 optimizer.zero_grad()
