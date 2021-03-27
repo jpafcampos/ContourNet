32ca1489ad6b75cb8df6c96151f24c9b22d0a62c
Joao Pedro Araujo Ferreira Campos <jpafcampos@gmail.com>
2021-03-27 18:54:45 +0100

    update

--- model/Net.py
+++ model/Net.py
@@ -25,9 +25,9 @@ class DilateConv(nn.Module):
     def forward(self, x):
         return self.d_conv(x)
 
-class RCF(nn.Module):
+class Net(nn.Module):
     def __init__(self):
-        super(RCF, self).__init__()
+        super(Net, self).__init__()
         #lr 1 2 decay 1 0
         self.conv1_1 = nn.Conv2d(3, 64, 3, padding=1)
         self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
